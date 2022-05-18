3470c98a03dad9c4c011257969b38cc58337437c
Murat TAMCI <m.tamci@gmail.com>
2022-05-18 17:41:51 +0300

    Add files via upload

---
+++ rHref Demo HTML/demo.cmd
@@ -0,0 +1,1 @@
+start rHref.exe -settings settings.ini
---
+++ rHref Demo HTML/dev mode.cmd
@@ -0,0 +1,3 @@
+@echo OFF
+echo in this Development command is false in settings.ini but now it's enable with -dev
+start rHref.exe -dev "themt://Run also this.xyz"
